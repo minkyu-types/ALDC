dc3b2f34ca5b67e418fd9935dc1c11caa1c3eec1
김민규 <mkkim96@dosystem.kr>
2026-01-05 14:57:49 +0900

    feat: 동기부여 객체 카테고리, 우선순위 ENUM 클래스 추가

---
+++ domain/src/commonMain/kotlin/dev/loki/domain/type/Category.kt
@@ -0,0 +1,12 @@
+package dev.loki.domain.type
+
+enum class Category(val title: String) {
+    HEALTH_FITNESS("건강・운동"),
+    CAREER_LEARNING("커리어・공부"),
+    FINANCE("경제"),
+    RELATIONSHIPS_COMMUNITY("관계・커뮤니티"),
+    MINDSET("마인드셋"),
+    PERSONAL_GROWTH("자기성장"),
+    CREATIVITY("창의성"),
+    HOBBY("취미")
+}
---
+++ domain/src/commonMain/kotlin/dev/loki/domain/type/Priority.kt
@@ -0,0 +1,7 @@
+package dev.loki.domain.type
+
+enum class Priority(val title: String) {
+    LOW("낮음"),
+    NORMAL("보통"),
+    HIGH("높음"),
+}
